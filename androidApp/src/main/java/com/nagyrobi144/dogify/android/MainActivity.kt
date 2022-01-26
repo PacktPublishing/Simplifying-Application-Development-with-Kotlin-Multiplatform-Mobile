8dce382463f1f5e868ccd3d97af43d7843e6e241
nrobi144 <robert.nagy.dev@gmail.com>
2022-01-26 11:23:45 +0200

    Fix build error

--- androidApp/src/main/java/com/nagyrobi144/dogify/android/MainActivity.kt
+++ androidApp/src/main/java/com/nagyrobi144/dogify/android/MainActivity.kt
@@ -11,20 +11,5 @@ import com.nagyrobi144.dogify.usecase.GetBreedsUseCase
 import com.nagyrobi144.dogify.usecase.ToggleFavouriteStateUseCase
 import kotlinx.coroutines.launch
 
-suspend fun greet() =
-    "${FetchBreedsUseCase().invoke()}\n" +
-            "${GetBreedsUseCase().invoke()}\n" +
-            "${ToggleFavouriteStateUseCase().invoke(Breed("toggle favourite state test", ""))}\n"
-
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-
-        lifecycleScope.launch {
-            tv.text = greet()
-        }
-    }
 }
